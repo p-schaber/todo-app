a06f829570b7ef811afedae82e014016be89c6dd
Peter Schaber <peteschaber@gmail.com>
2020-10-20 19:45:10 -0500

    Added state to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
-function TodoItem() {
-    return (
+// function TodoItem(props) {}
+const TodoItem = (props) => {
+
+    /** Array Destructuring
+     * var checkStateVariable = useState(false) // Returns a pair
+     * var isChecked = checkStateVariable[0] // First item in a pair
+     * var setCheck = checkStateVariable[1] // Second item in a pair
+     */
+    const [isChecked, setCheck] = useState(false)
+
+    return(
         <div className="todo-item">
-            <input type="checkbox"/>
-            <p>Some placeholder text here</p>
+            <input 
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => setCheck(isChecked ? false : true)}/>
+            <p>{props.item.text}</p>
+            <button>Click here!</button>
         </div>
     )
 }
---
+++ src/todosData.js
@@ -0,0 +1,30 @@
+const todosData = [
+
+    {
+        id: 1,
+        text: "Do the dishes",
+        complete: false,
+    },
+    {
+        id: 2,
+        text: "Wipe down the counters, sink, stove",
+        complete: false,
+    },
+    {
+        id: 3,
+        text: "Put shoes away",
+        complete: true,
+    },
+    {
+        id: 4,
+        text: "Tidy up living room",
+        complete: true,
+    },
+    {
+        id: 5,
+        text: "Fill the water",
+        complete: false,
+    },
+]
+
+export default todosData
